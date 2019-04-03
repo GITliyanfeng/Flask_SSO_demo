b9048443a53732942f22f9af356a424300a4b89a
liyanfeng <957012678@qq.com>
2019-04-03 23:26:43 +0800

    实现aw,bw两个站点的单点登陆

---
+++ bw.py
@@ -0,0 +1,31 @@
+import os
+
+from datetime import timedelta
+from flask import Flask, session, redirect, url_for, request
+import requests
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.permanent_session_lifetime = timedelta(seconds=24 * 60 * 60)
+
+
+@app.route('/')
+def index():
+    session.permanent = False
+    ticket = request.args.get('ticket', None)
+    if ticket is not None:
+        session['name'] = ticket.strip()
+    # 检测当前站点的登陆状态session会话中是否有name
+    if 'name' in session:
+        return 'bw成功登陆'
+    else:
+        referer = requests.utils.quote('http://www.aw.com:5001/')
+        return redirect('http://www.sso.com:5003/login?referer=' + referer)
+
+
+if __name__ == '__main__':
+    app.run(
+        host='127.0.0.1',
+        port=int('5001'),
+        debug=True
+    )
